dafc23c3474f3c1e103f33fc7da375141903b4fa
youliali <2901699151@qq.com>
2024-10-08 19:11:47 +0900

    hw2
    hw2

---
+++ 2024_fall/lec02/my_first_script.py
@@ -0,0 +1,13 @@
+""" 
+This is a script that I am creating as part of lecture 2 in the Speech Understanding course.
+"""
+
+print("Hello world")
+
+
+print(5+6) 
+print(9-4) 
+print(9/3) 
+print(5**3) 
+print(13%5) 
+print(13//5) 
