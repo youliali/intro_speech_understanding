8bc9928dc3cc60bef1a71ae0b6f35c188ef63f1c
youliali <2901699151@qq.com>
2024-12-21 23:43:31 +0900

    Update homework11.py

--- 2024_fall/lec11/homework11.py
+++ 2024_fall/lec11/homework11.py
@@ -13,4 +13,9 @@ def transcribe_wavefile(filename, language='en'):
     @returns:
     text (str) - the recognized speech
     '''
-    raise RuntimeError("FAIL!!  You need to change this function so it works!")
+    r = sr.Recognizer()
+    with sr.AudioFile(filename, language=language) as source:
+        audio = r.record(source)
+        text = r.recognize_google(audio)
+
+    return text
